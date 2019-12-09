fe29a9bc2509cb2755d062fd3bc718fc41cbaf54
microcompiler <terice@yahoo.com>
2019-12-08 20:45:51 -0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
+dotnet --info
 dotnet restore --verbosity --verbosity Minimal
 dotnet build --no-restore --verbosity Minimal --configuration Release
-dotnet test --no-restore --no-build --verbosity Minimal --configuration Release --results-directory .builds/artifacts --logger:trx -p:CollectCoverage=true -p:Exclude="[xunit.*]*" -p:CoverletOutputFormat=cobertura
+dotnet test --no-restore --no-build --verbosity Minimal --configuration Release --results-directory .builds/artifacts --logger:trx
 dotnet pack -p:SymbolPackageFormat=snupkg --no-restore --no-build --include-symbols --verbosity Minimal --configuration Release --output .builds/artifacts
