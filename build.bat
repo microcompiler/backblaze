eec9c5dc5ba7f9788544dc781a68672f0141a3d2
microcompiler <terice@yahoo.com>
2019-12-15 16:44:08 -0800

    Update action.yml

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ dotnet --info
 dotnet restore --verbosity --verbosity Minimal
 dotnet build --no-restore --verbosity Minimal --configuration Release
 dotnet test --no-restore --no-build --verbosity Minimal --configuration Release --results-directory .builds/artifacts --logger:trx
-dotnet pack -p:SymbolPackageFormat=snupkg --no-restore --no-build --include-symbols --verbosity Minimal --configuration Release --output .builds/artifacts
+dotnet pack --no-restore --no-build --include-symbols --verbosity Minimal --configuration Release --output .builds/artifacts
